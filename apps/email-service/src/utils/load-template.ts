fb176cb6a1334135f165d035e6ef183df01c0296
nhiennhatt <hiennhatt1804@gmail.com>
2026-08-06 10:22:56 +0700

    chore[email-service]: move template folder

--- apps/email-service/src/utils/load-template.ts
+++ apps/email-service/src/utils/load-template.ts
@@ -3,13 +3,7 @@ import Handlebars from 'handlebars';
 import path from 'path';
 
 export async function loadTemplate(name: string, data: unknown) {
-  const templatePath = path.join(
-    __dirname,
-    '..',
-    '..',
-    'templates',
-    `${name}.hbs`,
-  );
+  const templatePath = path.join(process.cwd(), 'templates', `${name}.hbs`);
   const templateSource = await readFile(templatePath, 'utf-8');
   const template = Handlebars.compile(templateSource);
   return template(data);
